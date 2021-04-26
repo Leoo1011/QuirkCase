ddde6d0fc13fe946890c4315198b10fda75bce9c
Leandro Araujo Gonzalez <43557954+Leoo1011@users.noreply.github.com>
2021-04-26 00:34:31 -0300

    Add conditions for non <input> elements
    Added a condition to check whether the target element is an input of type text or a textarea element. Also added code to add the transformed text to other elements (as long as they have the contenteditable attribute).

--- QuirkCase.js
+++ QuirkCase.js
@@ -4,14 +4,30 @@ window.onclick = (event) => {
         let text = prompt("eNtEr YoUr TeXt, PlEaSe.").split(""); // Split string into array
         let upOrLow = Math.random() >= 0.5; // Randomize first character's case
 
-        text.forEach( (char) => { // Loop through the array
-            if (upOrLow) {
-                target.value += char.toUpperCase(); // Change to uppercase...
+        if (target.tagName === "INPUT" && target.type === "text" || target.type === "textarea") {
+            // Code for text inputs
+            text.forEach( (char) => { // Loop through the array
+                if (upOrLow) {
+                    target.value += char.toUpperCase(); // Change to uppercase...
+                }
+                else {
+                    target.value += char.toLowerCase(); // ...or lowercase accordingly
+                }
+                upOrLow = !upOrLow; // Toggle the next character's case
+            });
+        }
+        else { // Code for other types of (text) elements (i.e. any element with the contenteditable attribute)
+            if (target.isContentEditable) {
+                text.forEach( (char) => {
+                    if (upOrLow) {
+                        target.innerText += char.toUpperCase();
+                    }
+                    else {
+                        target.innerText += char.toLowerCase();
+                    }
+                    upOrLow = !upOrLow;
+                });
             }
-            else {
-                target.value += char.toLowerCase(); // ...or lowercase accordingly
-            }
-            upOrLow = !upOrLow; // Toggle the next character's case
-        });
+        }
     }
 }
