1b2bc32d480971007949cda0c94d1618c3b8dd05
Leandro Araujo Gonzalez <43557954+Leoo1011@users.noreply.github.com>
2022-06-26 18:43:21 -0300

    Refactor and improve comments.
    Removed redundant comments and extracted out common pieces of the previous code into a function.

--- QuirkCase.js
+++ QuirkCase.js
@@ -1,33 +1,24 @@
-window.onclick = (event) => {
-    if (event.ctrlKey === true) {
-        let target = event.target; // The clicked element
-        let text = prompt("eNtEr YoUr TeXt, PlEaSe.").split(""); // Split string into array
-        let upOrLow = Math.random() >= 0.5; // Randomize first character's case
+function turnToQuirkCase(text) {
+    let newText = "";
+    let upOrLow = Math.random() >= 0.5;
+    text.forEach(char => {
+        newText += upOrLow ? char.toUpperCase() : char.toLowerCase();
+        upOrLow = !upOrLow; // Toggle the next character's case
+    });
+
+    return newText;
+}
+
+window.onclick = event => {
+    if (event.ctrlKey) {
+        const target = event.target;
+        const text = prompt("eNtEr YoUr TeXt, PlEaSe.").split("");
 
         if (target.tagName === "INPUT" && target.type === "text" || target.type === "textarea") {
-            // Code for text inputs
-            text.forEach( (char) => { // Loop through the array
-                if (upOrLow) {
-                    target.value += char.toUpperCase(); // Change to uppercase...
-                }
-                else {
-                    target.value += char.toLowerCase(); // ...or lowercase accordingly
-                }
-                upOrLow = !upOrLow; // Toggle the next character's case
-            });
+            target.value += turnToQuirkCase(text);
         }
-        else { // Code for other types of (text) elements (i.e. any element with the contenteditable attribute)
-            if (target.isContentEditable) {
-                text.forEach( (char) => {
-                    if (upOrLow) {
-                        target.innerText += char.toUpperCase();
-                    }
-                    else {
-                        target.innerText += char.toLowerCase();
-                    }
-                    upOrLow = !upOrLow;
-                });
-            }
+        else if (target.isContentEditable) {
+            target.innerText += turnToQuirkCase(text);
         }
     }
 }
